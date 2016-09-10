d55a4e3870be35361e2781cae3a28b81ee689e74
Alexander Pacha <alexander.pacha@gmail.com>
2016-09-10 17:06:44 +0200

    Added test-project to automatically test a few scenarios (including the invalid range items from #2).

--- RangeTreeExamples/RangeItem.cs
+++ RangeTreeExamples/RangeItem.cs
@@ -44,6 +44,40 @@ namespace RangeTreeExamples
         {
             return string.Format("{0} ({1} - {2})", Text, Range.From, Range.To);
         }
+
+        protected bool Equals(RangeItem other)
+        {
+            return string.Equals(Text, other.Text) && Range.Equals(other.Range);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return Equals((RangeItem)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Text != null ? Text.GetHashCode() : 0) * 397) ^ Range.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(RangeItem left, RangeItem right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(RangeItem left, RangeItem right)
+        {
+            return !Equals(left, right);
+        }
     }
 
     /// <summary>
---
+++ RangeTreeTests/InvalidRangeItemTests.cs
@@ -0,0 +1,20 @@
+﻿namespace RangeTreeTests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using RangeTreeExamples;
+
+    [TestFixture]
+    public class InvalidRangeItemTests
+    {
+        [Test]
+        public void ConstructNewRange_ProvideInvalidRange_ExpectArgumentOutOfRangeException()
+        {
+            // Act & Assert
+            Assert.That(() => new RangeItem(2, 1), Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.That(() => new RangeItem(2, 1, ""), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
